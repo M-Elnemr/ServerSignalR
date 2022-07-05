bb6c945e26a2aee6d0ad483baca885faa073a909
Mahmoud.Elnemr <developer.elnemr@gmail.com>
2022-07-05 14:19:23 +0200

    Add project files.

---
+++ ServerSignalR/Hubs/MoveViewHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerSignalR.Hubs
+{
+    public class MoveViewHub: Hub
+    {
+
+        public async Task MoveViewFromServer(String message)
+        {
+            Console.WriteLine(message);
+            await Clients.All.SendAsync("RecieveNewMessage", message);
+
+        }
+
+
+    }
+}
